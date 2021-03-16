83eb7987346a64af49dca7f80c107895906efa88
Badri Narayanan <programmingwithbadri@outlook.com>
2021-03-16 20:24:51 +0530

    Add a searchScreen file with the react-navigation

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <Text style={styles}>Search Screen</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+});
+
+export default SearchScreen;
