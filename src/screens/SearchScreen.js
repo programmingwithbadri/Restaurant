ea75ca7db5b046db88805d92b6fc15454ca60eab
Badri Narayanan <programmingwithbadri@outlook.com>
2021-03-17 18:39:13 +0530

    Show the search results with the groupByPrice in the ResultsList component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ResultsList from '../components/ResultsList';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, error] = useResults();
-    
+
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => result.price === price);
+    }
+
     return (
         <View>
             <SearchBar
@@ -15,7 +20,9 @@ const SearchScreen = () => {
                 onTermSubmit={() => searchApi(term)}
             />
             {error ? <Text>{error}</Text> : null}
-            <Text>We have found {results.length} results</Text>
+            <ResultsList title="Cost Effective" results={filterResultsByPrice('$')}/>
+            <ResultsList title="Bit Pricier" results={filterResultsByPrice('$$')}/>
+            <ResultsList title="Big Spender" results={filterResultsByPrice('$$$')}/>
         </View>
 
     )
