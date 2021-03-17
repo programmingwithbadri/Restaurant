39fe2651aa7f30fc3b2c4d78599a290e35ace76a
Badri Narayanan <programmingwithbadri@outlook.com>
2021-03-17 14:47:18 +0530

    Make the api request to yelp with the search bar input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = ({ term, onTermChange }) => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
     return (
         <View style={styles.backgroundStyle}>
             <Feather name='search' style={styles.iconStyle} />
@@ -13,6 +13,7 @@ const SearchBar = ({ term, onTermChange }) => {
                 placeholder='search'
                 value={term}
                 onChangeText={(text) => onTermChange(text)}
+                onEndEditing={() => onTermSubmit()}
             />
         </View>
     )
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,15 +1,33 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import yelp from '../api/yelp';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+
+    const searchApi = async () => {
+        const response = await yelp.get('/search', {
+            params: {
+                term,
+                limit: 50,
+                location: 'san jose'
+            }
+        });
+        setResults(response.data.businesses);
+    }
     return (
         <View>
-            <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} />
+            <SearchBar
+                term={term}
+                onTermChange={(newTerm) => setTerm(newTerm)}
+                onTermSubmit={() => searchApi()}
+            />
             <Text style={styles}>Search Screen</Text>
+            <Text>We have found {results.length} results</Text>
         </View>
-       
+
     )
 };
 
